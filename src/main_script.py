4e87e5fc2e26819eeedb771428cc7a556678009c
Austin Zimmerman <austinzimmerman@Austins-Laptop.local>
2020-04-30 18:09:53 -0600

    created files

---
+++ src/main_script.py
@@ -0,0 +1,57 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+import importlib
+from src.pipeline import Pipeline
+# importlib.reload(src.pipeline)
+# from src.pipeline import Pipeline
+
+
+
+def plot_corr_matrix(df):
+    f = plt.figure(figsize=(19, 15))
+    plt.matshow(df.corr(), fignum=f.number)
+    plt.xticks(range(df.shape[1]), df.columns, fontsize=14, rotation=45)
+    plt.yticks(range(df.shape[1]), df.columns, fontsize=14)
+    cb = plt.colorbar()
+    cb.ax.tick_params(labelsize=14)
+    plt.title('Correlation Matrix', fontsize=16)
+
+
+
+
+
+if __name__ == '__main__':
+    # energy_df = pd.read_csv('data/energy_dataset.csv',index_col=0, parse_dates=[0])
+    # weather_df = pd.read_csv('data/weather_features.csv',index_col=0, parse_dates=[0])
+    print("Loading Data")
+    energy = Pipeline('data/energy_dataset.csv')
+    weather = Pipeline('data/weather_features.csv')
+    # energy_df.index = pd.to_datetime(energy_df.index, utc=True)
+    # weather_df.index = pd.to_datetime(weather_df.index, utc=True)
+    
+    merged_df = energy.merge_dfs(weather.df)
+    weather.consolidate('dt_iso')
+    merged_by_date = energy.merge_dfs(weather.grouped_avg)
+    merged_by_date.getXy('price actual')
+    merged_by_date.create_holdout()
+    
+    # energy_df.merge(weather_df, right_index=True, left_index=True)  
+
+    # cities_gb = weather_df.groupby("dt_iso")
+    # cities_avg = cities_gb.mean()
+
+    # plt.matshow(energy_df.corr())
+    # plt.show()
+    # print('creating matrices')
+    # plot_corr_matrix(energy_df)
+    # plt.savefig('images/energy_corr.png')
+    # plt.close()
+    # plot_corr_matrix(weather_df)
+    # plt.savefig('images/weather_corr.png')
+    # plt.close()
+    print('all done.')
+
+
+    
---
+++ src/pipeline.py
@@ -0,0 +1,60 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+
+
+class Pipeline(object):
+
+    def __init__(self, path):
+        self.df = pd.read_csv(path,index_col=0, parse_dates=[0])
+        
+        # X and y values to be assigned when create_holdout() is run
+        self.X = None
+        self.y = None
+
+        self.X_train = None
+        self.y_train = None
+
+        self.X_test = None
+        self.y_test = None
+
+        self.X_holdout = None
+        self.y_holdout = None
+
+        # to be assigned if a groupby is necessary
+        self.grouped_avg = None
+
+    @classmethod
+    def from_df(cls, df):
+        obj = cls.__new__(cls)
+        super(Pipeline, obj).__init__()
+        obj.df = df
+        return obj
+
+    def read(self,path):
+        pass
+
+    def reset_index(self):
+        self.df.index = pd.to_datetime(self.df.index, utc=True)
+
+    def merge_dfs(self,new_df):
+        return Pipeline.from_df(self.df.merge(new_df, right_index=True, left_index=True))
+
+    def getXy(self,target):
+        "Target (string): name of the column that contains the y values"
+        self.y = self.df.pop(target)
+        self.X = self.df
+        return self.X, self.y
+
+    def consolidate(self, group_on):
+        gb = self.df.groupby(group_on)
+        self.grouped_avg = gb.mean()
+        
+
+    def create_holdout(self):
+        X_cv, self.X_holdout, y_cv, self.y_holdout = train_test_split(self.X,self.y)
+        self.X_train, self.X_test, self.y_train, self.y_test = \
+            train_test_split(X_cv, y_cv)
+
+        return  self.X_train, self.y_train, self.X_test, self.y_test, self.X_holdout, self.y_holdout
