95852296abca02bbb03d1d663af0249e55e9b884
Austin Zimmerman <austinzimmerman@Austins-Laptop.local>
2020-05-12 11:43:37 -0600

    full corr matrix

--- src/main_script.py
+++ src/main_script.py
@@ -38,24 +38,19 @@ if __name__ == '__main__':
     energy.my_reset_index()
     weather.my_reset_index()
 
-# full_df.df.loc[full_df.df[' Barcelona_pressure'] > 1040, ' Barcelona_pressure'] = np.nan
-
-# weather.df.loc[weather.df.pressure > 1040, 'pressure'] = np.nan
-# weather.df.pressure.fillna(method='pad', inplace=True)
-
     # Drop columns
     weather_drop_cols = ['weather_icon', 'weather_main', 'weather_id']
     energy_drop_cols = ['generation fossil coal-derived gas','generation fossil oil shale', 
                         'generation fossil peat', 'generation geothermal',
                         'generation marine', 'generation hydro pumped storage aggregated',
-                         'forecast wind offshore eday ahead']
+                         'forecast wind offshore eday ahead', 'generation wind offshore']
 
     for i in weather_drop_cols:
         weather.df.drop(i, axis=1, inplace=True)
     for i in energy_drop_cols:
         energy.df.drop(i, axis=1, inplace=True)
 
-    # propagate last valid observation forward to next valid
+    # propagate last valid observation forward to next valid to fill NaNs
     for i in energy.df.columns:
         energy.df[i].fillna(method='pad', inplace=True)
 
@@ -91,12 +86,16 @@ if __name__ == '__main__':
     barcelona = Pipeline.from_df(city_df_list[3])
     sevilla = Pipeline.from_df(city_df_list[4])
 
+    # There has GOT to be a better way to do this
     vm = valencia.merge_dfs(madrid.df)
     bb = bilbao.merge_dfs(barcelona.df)
     sbb = sevilla.merge_dfs(bb.df)
     all_cities_df = vm.merge_dfs(sbb.df)
 
-    
+    # clean residual col names that came from the merge
+    for i in ["Valencia_city_name", " Barcelona_city_name", "Bilbao_city_name", 
+            "Seville_city_name", "Madrid_city_name"]:
+        all_cities_df.df.drop(i, axis=1, inplace=True)
 
     
     # Transformations
@@ -105,44 +104,15 @@ if __name__ == '__main__':
     # weather.clean_categoricals(weather_cols)
     energy_cols = []
 
-
+    # Merge energy with the featurized cities DF to make the complete DataFrame
     full_df = energy.merge_dfs(all_cities_df.df)
-
-
-    for i in ["Valencia_city_name", " Barcelona_city_name", "Bilbao_city_name", 
-            "Seville_city_name", "Madrid_city_name"]:
-        full_df.df.drop(i, axis=1, inplace=True)
-
-
-    weather.consolidate('dt_iso')
-    merged_by_date = energy.merge_dfs(weather.grouped_avg)
-
-    # weather.df.set_index('city_name', append=True, inplace=False, drop=False)
-
-    # test = test.reset_index()
-
-    # test.set_index(['dt_iso'])
-    # weather.df.set_index('city_name', append=True)
-
-# for feat in ["Madrid", "Valencia"]:
-#     for col in weather.df.columns:
-#         weather.df[f"{feat}_{col}"] = weather.df[col][weather.df['city_name'] == "Madrid"]
-
-# cols = test.columns
-# for feat in ["Madrid", "Valencia"]:
-#     for col in cols[1:]:
-#         test[f"{feat}_{col}"] = test[col][test['city_name'] == feat]
-
-
-
-
 
 
     print('\nCreating train, test, and holdout sets')
     full_df.getXy('price actual')
     full_df.create_holdout()
 
-    # print('\nWriting train, test, and holdouts to filesystem')
+    print('\nWriting train, test, and holdouts to filesystem')
 
     train_test_split_holdout_list = [full_df.X_train, full_df.X_test, 
                                     full_df.X_holdout, full_df.X_std, full_df.y_train, 
@@ -156,9 +126,9 @@ if __name__ == '__main__':
     # for (i, fname) in zip(train_test_split_holdout_list, ttsh_filenames):
     #         i.to_csv(f"data/{fname}.csv")
    
-    for (i, fname) in zip(train_test_split_holdout_list, ttsh_filenames):
-        if type(i) == 'numpy.ndarray':
-            i.to_csv(f's3://ajzcap2/{fname}.csv')
+    # for (i, fname) in zip(train_test_split_holdout_list, ttsh_filenames):
+    #     if type(i) == 'numpy.ndarray':
+    #         i.to_csv(f's3://ajzcap2/{fname}.csv')
    
 
     
@@ -166,14 +136,18 @@ if __name__ == '__main__':
   
 
     
-    # plot_corr_matrix(energy.df)
-    # plt.savefig('images/clean_energy_corr.png')
-    # # plt.show()
-    # plt.close()
-    # plot_corr_matrix(weather.df)
-    # plt.savefig('images/clean_weather_corr.png')
-    # # plt.show()
-    # plt.close()
+    plot_corr_matrix(energy.df)
+    plt.savefig('images/clean_energy_corr.png')
+    # plt.show()
+    plt.close()
+    plot_corr_matrix(weather.df)
+    plt.savefig('images/clean_weather_corr.png')
+    # plt.show()
+    plt.close()
+
+    plot_corr_matrix(full_df.df)
+    plt.savefig('images/full_corr.png')
+    plt.close()
     
     print('all done.')
 
